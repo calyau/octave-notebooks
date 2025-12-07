083267e53a564ba342ba63a984b3608d242a44c4
Duncan McGreggor <oubiwann@gmail.com>
2025-12-07 12:58:59 -0600

    Added missing octave files.

---
+++ data/packages/custom-util/fs.m
@@ -0,0 +1,11 @@
+function ensure_dirs(filepath)
+  % Extract directory from filepath
+  [dir, name, ext] = fileparts(filepath);
+
+  % Create directory if it doesn't exist
+  if !exist(dir, 'dir')
+    mkdir(dir)
+  endif
+endfunction
+
+printf('File system functions loaded!\n');
---
+++ data/packages/custom-util/strings.m
@@ -0,0 +1,21 @@
+function result = titlecase(str)
+  % Convert to lowercase first
+  str = lower(str);
+
+  % Make the first character uppercase
+  if length(str) > 0
+    str(1) = upper(str(1));
+  endif
+
+  % Find spaces and capitalize the letter after each space
+  spaces = find(str == ' ');
+  for i = 1:length(spaces)
+    if spaces(i) < length(str)
+      str(spaces(i) + 1) = upper(str(spaces(i) + 1));
+    endif
+  endfor
+
+  result = str;
+endfunction
+
+printf('String functions loaded!\n');
